adcbdb67dc99024076c5413c02463b8bbe27aefc
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-10 18:03:52 +0530

    Create 2.c

---
+++ 23-01-10/2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	int T,N;
+	scanf("%d",&T);
+	while(T--){
+	    scanf("%d",&N);
+	    int a[N];
+	    int min;
+	    for(int i=0;i<1;i++){
+	        scanf("%d",&a[i]);
+	        min=a[i];
+	    }
+	    for(int i=1;i<N;i++){
+	        scanf("%d",&a[i]);
+	        if(a[i]<min){
+	            min=a[i];
+	        }
+	    }
+	    int sum=0;
+	    int c=0;
+	    for(int i=0;i<N;i++){
+	        if(a[i]==min && c==0){
+	            c++;
+	            continue;
+	        }
+	        sum=sum+a[i];
+	    }
+	    printf("%d\n",sum);
+	    
+	}
+	return 0;
+}
+
