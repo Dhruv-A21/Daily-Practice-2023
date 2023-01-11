295b6e0b7d19a0baa93b276d529b5933e456bfe5
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-01-11 20:03:09 +0530

    Create 02.c

---
+++ 23-01-11/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int T,N;
+	scanf("%d",&T);
+	while(T--){
+	    scanf("%d",&N);
+	    if(N==6){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
