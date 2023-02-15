e2b9d760b8ee208b490ccb094c9962b6d2a77cc1
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-02-15 22:43:50 +0530

    Create 3.c

---
+++ 23-02-15/3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int min(int a,int b){
+    int m=a;
+    if(m>b){
+        m=b;
+    }
+    return (m);
+}
+int main(void) {
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int A,B,C;
+	    scanf("%d %d %d",&A,&B,&C);
+	    
+	}
+	return 0;
+}
