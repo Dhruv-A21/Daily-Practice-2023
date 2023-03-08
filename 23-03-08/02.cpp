093bacdf847f8fa17bc9acdf86c03e44fe1768cb
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-03-08 20:01:45 +0530

    Create 02.cpp

---
+++ 23-03-08/02.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>24){
+	        printf("Yes\n");
+	    }
+	    else{
+	        printf("No\n");
+	    }
+	}
+	return 0;
+}
