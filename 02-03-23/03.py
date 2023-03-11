571e5e4a2172264dc269429cb9ae9cf09d8c5df8
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-03-11 21:57:50 +0530

    Delete 02-03-23 directory

--- 02-03-23/03.py
+++
@@ -1,7 +0,0 @@
-a=int(input())
-b=int(input())
-for i in range ((a//2)+1):
-    j=a-i
-    if((i^j)==b):
-        print(i,j)
-        
--- 02-03-23/04.py
+++
@@ -1,9 +0,0 @@
-import math
-a=int(input())
-dig=(int(math.log2(a)))+1
-for i in range (0,a+1):
-    s=bin(i)
-    p=s[2:]
-    c=len(p)
-    print((dig-c)*" ",end="")
-    print(p)
