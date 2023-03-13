a85040a2d97b6fa92b66195469571dbeabb2e7ef
Dhruv Agrawal <114812009+Dhruv-A21@users.noreply.github.com>
2023-03-13 23:44:26 +0530

    Create 07.cpp

---
+++ 23-03-13/07.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+	int T,N;
+	cin>>T;
+	while(T--){
+	    cin>>N;
+	    int a[N];
+	    int max=0;
+	    for(int i=0;i<N;i++){
+	        cin>>a[i];
+	        if(max<a[i]){
+	            max=a[i];
+	        }
+	    }
+	    max++;
+	    int f[max]={0};
+	    for(int i=0;i<N;i++){
+	        f[a[i]]++;
+	    }
+	    int s=0;
+	    int o=0;
+	    for(int i=0;i<max;i++){
+	        if(f[i]==1){
+	            s++;
+	        }
+	        if(f[i]%2!=0){
+	            o++;
+	        }
+	    }
+	    if(f[max-1]==1){
+	        if(s==1 && o==1){
+	            s=3;
+	        }
+	    }
+	    if(s%2!=0){
+	        s++;
+	    }
+	    cout<<s/2<<endl;
+	}
+	return 0;
+}
